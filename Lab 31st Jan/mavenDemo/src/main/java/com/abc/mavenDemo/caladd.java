f017534b2d4b6bf0c7d1768e473e57825a0e3b4b
Sourabh Kondapaka <sourabhkondapaka@gmail.com>
2019-02-04 13:07:50 +0530

    31st Lab session

---
+++ Lab 31st Jan/mavenDemo/src/main/java/com/abc/mavenDemo/caladd.java
@@ -0,0 +1,72 @@
+package com.abc.mavenDemo;
+
+import java.util.Scanner;
+
+public class caladd {
+
+
+    public static void main(String[] args) {
+
+        float a,b,res;
+        char choice,ch;
+        
+       
+
+        Scanner scanner = new Scanner(System.in);
+
+        do {
+
+            System.out.println("1. Add two numbers \n");
+
+            System.out.println("2. Multiply 2 numbers two numbers \n");
+
+            System.out.println("3. Subtract two numbers \n");
+
+
+            System.out.println("9. Exit \n\n");
+            System.out.println("Enter your choice :");
+
+            choice = scanner.next().charAt(0);
+
+            switch (choice)
+            {
+                case '1':   System.out.println("Enter two numbers");
+                            a = scanner.nextFloat();
+                            b = scanner.nextFloat();
+                            res = a + b;
+                            System.out.println("Result is " + res);
+                            break;
+
+
+                case '2':   System.out.println("Enter two numbers");
+                            a = scanner.nextFloat();
+                            b = scanner.nextFloat();
+                            res = a * b;
+                            System.out.println("Result is " + res);
+                            break;
+
+
+                 case '3':   System.out.println("Enter two numbers");
+                            a = scanner.nextFloat();
+                            b = scanner.nextFloat();
+                            res = a - b;
+                            System.out.println("Result is " + res);
+                            break;
+
+                case '9' : System.exit(0);
+                           break;
+
+
+                default:   System.out.println("Invalid Choice");
+                           break;
+            }
+
+            System.out.println("\n --------------- \n");
+            System.out.println("this line is added by team2");
+
+
+
+        }while (choice != 9);
+
+    }
+}
